485272ebfb77f692bbfb614af0098147efddd7f4
Zheng Lu <z5135652@cse.unsw.edu.au>
2018-10-01 12:21:34 +1000

    backend stuffs

--- workspace/app.js
+++ workspace/app.js
@@ -9,19 +9,32 @@ var express     = require("express"),
     methodOverride = require("method-override"),
     LocalStrategy = require("passport-local"),
     flash        = require("connect-flash"),
+
+    //Database models
     Coderoom  = require("./models/coderoom"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user");
+    // Comment     = require("./models/comment"),
+    User        = require("./models/user"),
 
     
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
+//     commentRoutes    = require("./routes/comments"),
+    indexRoutes      = require("./routes/index"),
     coderoomRoutes = require("./routes/coderooms"),
-    indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb://localhost/coderoom");
+    userRoutes = require("./routes/users"),
+    // fileRouters = require("./routes/pythonFiles"),
+    userRouter = require('./routes/users');
+
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://zen:a123321@ds139072.mlab.com:39072/ggwp';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
 app.use(cookieParser('secret'));
@@ -38,9 +51,28 @@ app.use(require("express-session")({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (user.password != password) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, {username: user.username, id: user._id});
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
@@ -49,11 +81,17 @@ app.use(function(req, res, next){
    next();
 });
 
-
+//Routers configration
+userRouter(app, passport);
 app.use("/", indexRoutes);
-app.use("/coderoom", coderoomRoutes);
-app.use("/coderooms/:id/comments", commentRoutes);
+app.use("/api/coderooms", coderoomRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/files", fileRouters);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The server has been started");
+
+//server configration
+port = process.env.PORT || 3000;
+ip = process.env.IP || '127.0.0.1';
+app.listen(port, ip, function(){
+   console.log(`The server has been started. Visiting address: http://${ip}:${port}.`);
 });
