075e4d86a3cf79f8e7b7fcce8de0eafd94328710
zen <lu345808591@gmail.com>
2018-10-02 01:45:01 +1000

    reduce lag, add change permission function

--- workspace/public/javascripts/myJS.js
+++ workspace/public/javascripts/myJS.js
@@ -1,109 +1,101 @@
-(function() {
-    //FireBase 设置
-    var config = {
-        apiKey: "AIzaSyDyFnZMXeY2gXJNSZe58tqbOkZX7-5yiDM",
-        authDomain: "comp9323-97bb4.firebaseapp.com",
-        databaseURL: "https://comp9323-97bb4.firebaseio.com",
-        projectId: "comp9323-97bb4",
-        storageBucket: "comp9323-97bb4.appspot.com",
-        messagingSenderId: "837737259182"
-    };
-    firebase.initializeApp(config);
-    console.log("Test.");
+//FireBase 设置
+var config = {
+    apiKey: "AIzaSyDyFnZMXeY2gXJNSZe58tqbOkZX7-5yiDM",
+    authDomain: "comp9323-97bb4.firebaseapp.com",
+    databaseURL: "https://comp9323-97bb4.firebaseio.com",
+    projectId: "comp9323-97bb4",
+    storageBucket: "comp9323-97bb4.appspot.com",
+    messagingSenderId: "837737259182"
+};
+firebase.initializeApp(config);
 
-    var roomId = '5bab7be8ade838281621911a';
+var permission = false;
+var roomId = '5bab7be8ade838281621911a';
+//CodeMirro Editor initialize
+//Need further changes for optimization.
+var editor = CodeMirror.fromTextArea(document.getElementById("code_input"), {
+    mode: "python",
+    theme: "darcula",
+    lineNumbers: true,
+    // autoMatchBrackets: true,
+    readOnly: !permission                 //set to true if user have the permission.
+});
+var cursorPos = {line: 0, ch: 0};
+//function to update code to firebase
+function updateCode() {
+    const roomId = '5bab7be8ade838281621911a';
+    firebase.database().ref().child('code/').child(roomId).update(
+        {'content': editor.getValue()}
+    );
+}
+
+//when editor's content changed, call updateCode()
+editor.on('change', () => {
+    cursorPos.line = editor.getCursor().line;
+    cursorPos.ch   = editor.getCursor().ch;
+    updateCode();
+    editor.setCursor(cursorPos);
+});
+
+const code = document.getElementById('code');
+const comment_list = document.getElementById('comment_list');
+const user_list = document.getElementById('user_list');
+
+//dbRefObject 是firebase根目录
+const dbRefObject = firebase.database().ref();
+const dbRefCommentList = dbRefObject.child('comment_list').child(roomId);
+const dbRefUserList = dbRefObject.child('user_list').child(roomId);
+const dbRefCode = dbRefObject.child('code').child(roomId);
+const code_input = document.getElementById('code_input');
 
 
-    const code = document.getElementById('code');
-    const comment_list = document.getElementById('comment_list');
-    const user_list = document.getElementById('user_list');
-
-    //dbRefObject 是firebase根目录
-    const dbRefObject = firebase.database().ref();
-    const dbRefCommentList = dbRefObject.child('comment_list').child(roomId);
-    const dbRefUserList = dbRefObject.child('user_list').child(roomId);
-    const dbRefCode = dbRefObject.child('code').child(roomId);
-    const code_input = document.getElementById('code_input');
-
-
-    //同步输入框
-    dbRefCode.on('value', snap => {
-        const content = snap.val();
-        code_input.value = content['content'];
+//同步输入框
+dbRefCode.on('value', snap => {
+    const content = snap.val();
+    code_input.value = content['content'];
+    if (!permission)
         editor.setValue(code_input.value);
-        //有问题，如果不是在文件底部修改的，那么每次都会跳转到文件底部
-        editor.execCommand("goDocEnd");
-    });
-    //同步code显示框
-    dbRefCode.on('value', snap => {
-        const content = snap.val();
-        code.innerText = content['content'];
-    });
+});
 
-    //同步user list, 因为user list是由子elem构成的所以按照子elem来增删（用户进入/离开房间）
-    //之后需在list中添加更多元素比如：请求permission（bool）（当permission转换时，所有请求permission的状态应该转成false，新一轮）， 
-    //拥有permission（bool）。。。。
-    dbRefUserList.on('child_added', snap => {
-        const li = document.createElement('li');
-        li.innerText = snap.val().username;
-        li.id = snap.key;
-        user_list.appendChild(li);
-    });
-    dbRefUserList.on('child_removed', snap => {
-        const liToRemove = document.getElementById(snap.key);
-        liToRemove.remove();
-    });
+//同步code显示框
+dbRefCode.on('value', snap => {
+    const content = snap.val();
+    code.innerText = content['content'];
+});
 
-    //同步comment list，与user list类似，但是comment可以改，user不行
-    //comment list 需要有：（作者, 内容, 子comment，。。。）
-    dbRefCommentList.on('child_added', snap => {
-        const li = document.createElement('li');
-        li.innerText = JSON.stringify(snap.val(), null, 3);
-        li.id = snap.key;
-        comment_list.appendChild(li);
-    });
+//同步user list, 因为user list是由子elem构成的所以按照子elem来增删（用户进入/离开房间）
+//之后需在list中添加更多元素比如：请求permission（bool）（当permission转换时，所有请求permission的状态应该转成false，新一轮），
+//拥有permission（bool）。。。。
+dbRefUserList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.val().username;
+    li.id = snap.key;
+    user_list.appendChild(li);
+});
 
-    dbRefCommentList.on('child_removed', snap => {
-        const liToRemove = document.getElementById(snap.key);
-        liToRemove.remove();
-    });
-    dbRefCommentList.on('child_changed', snap => {
-        const liChanged = document.getElementById(snap.key);
-        liChanged.innerText = JSON.stringify(snap.val());
-    });
+dbRefUserList.on('child_removed', snap => {
+    const liToRemove = document.getElementById(snap.key);
+    liToRemove.remove();
+});
 
-    //CodeMirro Editor initialize
-    //Need further changes for optimization.
-    var editor = CodeMirror.fromTextArea(document.getElementById("code_input"), {
-        mode: "python",
-        theme: "darcula",
-        lineNumbers: true,
-        // autoMatchBrackets: true,
-        // readOnly: false                 //set to true if user have the permission.
-    });
-    editor.focus();
-    //function to update code to firebase
-    function updateCode() {
-        editor.save();
-        const roomId = '5bab7be8ade838281621911a';
-        const code_input = document.getElementById('code_input');
-        firebase.database().ref().child('code/').child(roomId).update(
-            {'content': code_input.value}
-        );
-    }
-    //when editor's content changed, call updateCode()
-    editor.on('change', updateCode);
+//同步comment list，与user list类似，但是comment可以改，user不行
+//comment list 需要有：（作者, 内容, 子comment，。。。）
+dbRefCommentList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = JSON.stringify(snap.val(), null, 3);
+    li.id = snap.key;
+    comment_list.appendChild(li);
+});
 
-})();
+dbRefCommentList.on('child_removed', snap => {
+    const liToRemove = document.getElementById(snap.key);
+    liToRemove.remove();
+});
 
-
-// function updateCode() {
-//     const roomId = '5bab7be8ade838281621911a';
-//     const code_input = document.getElementById('code_input');
-//     firebase.database().ref().child('code/').child(roomId).update(
-//         {'content': code_input.value}
-//     );
-// }
+dbRefCommentList.on('child_changed', snap => {
+    const liChanged = document.getElementById(snap.key);
+    liChanged.innerText = JSON.stringify(snap.val());
+});
 
 function addComment() {
     const roomId = '5bab7be8ade838281621911a';
@@ -129,8 +121,8 @@ function addUser() {
     const user_input = document.getElementById('user_input');
     const newPostRef = firebase.database().ref().child('user_list/' + roomId).child(user.id)
         .update({
-            'username': user.username, 
-            'permission': false, 
+            'username': user.username,
+            'permission': false,
             'ask-for-permission': false,
             'url': url
         });
@@ -145,28 +137,32 @@ function removeUser(user) {
 
 function run() {
     const result = document.getElementById('result');
-    const roomId = '5bab7be8ade838281621911a'
+    const roomId = '5bab7be8ade838281621911a';
     const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/run';
     var jqxhr = $.ajax({
-        url: url, 
+        url: url,
         method: 'get',
         dataType: 'json'
     }).done(function (data) {
-        //json 格式输出
-        // result.innerText = JSON.stringify(data);
-        //string 输出
         if (data['output'])
-        	result.innerText = data['output'];
+            result.innerText = data['output'];
         else
-        	result.innerText = data['err'];
-
+            result.innerText = data['err'];
     }).fail(function (xhr, status) {
         result.innerText = 'Fail: ' + xhr.status + ', Reason: ' + status;
-    }).always(function () {
-        console.log("This is an ajex request");
     });
 }
 
+function change() {
+    dbRefCode.once('value', snap => {
+        const content = snap.val();
+        code_input.value = content['content'];
+    });
+    permission = !permission;
+    console.log("permission: ", permission);
+    editor.setValue(code_input.value);
+    editor.setOption("readOnly", !permission);
+}
 
 
 
