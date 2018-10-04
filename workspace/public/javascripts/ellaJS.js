80f8ffd773a17263e83265b7d2228a091a9d1c59
missmint27 <zyk493122273@163.com>
2018-10-05 09:43:32 +1000

    #1 move setting's js

--- workspace/public/javascripts/ellaJS.js
+++ workspace/public/javascripts/ellaJS.js
@@ -1,3 +1,24 @@
+/*setting get avatar*/
+$(document).ready(function () {
+
+
+    var readURL = function (input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $(".file-upload").on('change', function () {
+        readURL(this);
+    });
+});
 /*This part is for search bar*/
 function searchBar() {
     var x = document.getElementById("hidDiv");
