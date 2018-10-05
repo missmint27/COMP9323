423e99321f4ab97b6c6a7f55b2366865423cddf5
zen <lu345808591@gmail.com>
2018-10-05 11:07:29 +1000

    middleware isOwner

--- workspace/middleware/index.js
+++ workspace/middleware/index.js
@@ -6,18 +6,17 @@ module.exports = {
             return next();
         }
         req.flash("error", "You must be signed in to do that!");
-        res.redirect("/login");
+        res.status('400').json({'msg': 'Please Log in'});
     },
-    checkUserCoderoom: function(req, res, next){
-        if(req.isAuthenticated()){
+    isOwner: function(req, res, next){
+        if(req.user){
             Coderoom.findById(req.params.id, function(err, found_coderoom){
-               if(found_coderoom.permitted_user.id.equals(req.user._id)
-                   || found_coderoom.author.id.equals(req.user._id)
+               if(found_coderoom.permitted_user.id.equals(req.user.id)
+                   || found_coderoom.author.id.equals(req.user.id)
                    || req.user.isAdmin){
                    next();
                } else {
                    req.flash("error", "You don't have permission to do that!");
-                   console.log("BADD!!!");
                    res.redirect("/coderooms/" + req.params.id);
                }
             });
@@ -27,7 +26,6 @@ module.exports = {
         }
     },
     checkUserComment: function(req, res, next){
-        console.log("YOU MADE IT!");
         if(req.isAuthenticated()){
             Comment.findById(req.params.commentId, function(err, comment){
                if(comment.author.id.equals(req.user._id) || req.user.isAdmin){
@@ -39,7 +37,7 @@ module.exports = {
             });
         } else {
             req.flash("error", "You need to be signed in to do that!");
-            res.redirect("login");
+            res.redirect("/login");
         }
     }
-}
+};
