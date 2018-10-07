adc98c99fcc5d90c4f86f0431c744a6942ca2c50
zen <lu345808591@gmail.com>
2018-10-07 16:30:55 +1100

    formation

--- workspace/public/javascripts/myJS.js
+++ workspace/public/javascripts/myJS.js
@@ -1,194 +1,187 @@
- //FireBase 设置
-    var config = {
-        apiKey: "AIzaSyDyFnZMXeY2gXJNSZe58tqbOkZX7-5yiDM",
-        authDomain: "comp9323-97bb4.firebaseapp.com",
-        databaseURL: "https://comp9323-97bb4.firebaseio.com",
-        projectId: "comp9323-97bb4",
-        storageBucket: "comp9323-97bb4.appspot.com",
-        messagingSenderId: "837737259182"
-    };
-    firebase.initializeApp(config);
+//FireBase 设置
+var config = {
+    apiKey: "AIzaSyDyFnZMXeY2gXJNSZe58tqbOkZX7-5yiDM",
+    authDomain: "comp9323-97bb4.firebaseapp.com",
+    databaseURL: "https://comp9323-97bb4.firebaseio.com",
+    projectId: "comp9323-97bb4",
+    storageBucket: "comp9323-97bb4.appspot.com",
+    messagingSenderId: "837737259182"
+};
+firebase.initializeApp(config);
+//CodeMirro Editor initialize
+//Need further changes for optimization.
+var editor = CodeMirror.fromTextArea(document.getElementById("code_input"), {
+    mode: "python",
+    theme: "darcula",
+    lineNumbers: true,
+// autoMatchBrackets: true,
+    readOnly: !permission,                 //set to true if user have the permission.
+});
 
-    var permission = false;
-    var roomId = '5bab7be8ade838281621911a';//需要动态获取roomid
-    var userId = '5bb053d0efdfca206dc66b3b';
-    // var users = null;
-    // var ask_for_permission = null;
-    //CodeMirro Editor initialize
-    //Need further changes for optimization.
-    var editor = CodeMirror.fromTextArea(document.getElementById("code_input"), {
-        mode: "python",
-        theme: "darcula",
-        lineNumbers: true,
-        // autoMatchBrackets: true,
-        readOnly: !permission,                 //set to true if user have the permission.
-        // highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true}
-    });
-    //maintaining a global comment dict, it stores the marker of comment in the coding area, using commentId as key
-    var comment_dict = {};
-    //TODO when comment code is on, the comment mark should appear, the comment list should show.
-    var comment_mode = false;
-    //when editor's content changed, call updateCode()
-    editor.on('change', updateCode);
-    //return pos: {from: {line:int, ch:int}, to:{line: int, ch: int}}
-    editor.getSelectedRange = function() {
-        return { from: editor.getCursor(true), to: editor.getCursor(false) };
-    };
+var permission = false;
 
-    //dbRefObject 是firebase根目录
-    const dbRefObject = firebase.database().ref();
-    const dbRefCommentList = dbRefObject.child('comment_list').child(roomId);
-    const dbRefUserList = dbRefObject.child('user_list').child(roomId);
-    const dbRefCode = dbRefObject.child('code').child(roomId);
-    const dbRefRoomPermission = dbRefObject.child('permission/' + roomId);
-    //TODO if user is ask for permission, the button on the user list would appear;
-    const dbRefAskForPermission = dbRefObject.child('ask-for-permission/' + roomId);
+// const roomId = document.getElementById("roomId");
+// const userId = document.getElementById("userId");
+const code_input = document.getElementById('code_input');
+const comment_list = document.getElementById('comment_list');
+const user_list = document.getElementById('user_list');
 
-    const code_input = document.getElementById('code_input');
-    const code = document.getElementById('code');
-    const comment_list = document.getElementById('comment_list');
-    const user_list = document.getElementById('user_list');
+var roomId = '5bab7be8ade838281621911a';
+var userId = '5bb053d0efdfca206dc66b3b';
+// var users = null;
+// var ask_for_permission = null;
 
-    //track the permission status.
-    //if permission is true, set the editor readOnly to false, theme will be changed as well.
-    dbRefRoomPermission.on('value', snap => {
-        const permission_holder = snap.val().userId;
-        console.log("permission holder: ", permission_holder);
-        if (permission_holder !== userId && permission === false) {
-        } else {
-            permission = permission_holder === userId;
-            dbRefCode.once('value', codeSnap => {
-                const content = codeSnap.val();
-                //global var permission
-                code_input.value = content['content'];
-                editor.setValue(code_input.value);
-                editor.setOption("readOnly", !permission);
-                if(permission) {
-                    editor.setOption("theme", 'darcula');
-                } else {
-                    editor.setOption("theme", 'lucario');
-                }
+//maintaining a global comment dict, it stores the marker of comment in the coding area, using commentId as key
+var comment_dict = {};
+//change the global comment mode to shift display mode.
+var comment_mode = false;
+
+//when editor's content changed, call updateCode()
+editor.on('change', updateCode);
+
+//return pos: {from: {line:int, ch:int}, to:{line: int, ch: int}}
+editor.getSelectedRange = function() {
+    return { from: editor.getCursor(true), to: editor.getCursor(false) };
+};
+
+//dbRefObject is the root directory of firebase
+const dbRefObject = firebase.database().ref();
+const dbRefCommentList = dbRefObject.child('comment_list').child(roomId);
+const dbRefUserList = dbRefObject.child('user_list').child(roomId);
+const dbRefCode = dbRefObject.child('code').child(roomId);
+const dbRefRoomPermission = dbRefObject.child('permission/' + roomId);
+const dbRefAskForPermission = dbRefObject.child('ask-for-permission/' + roomId);
+
+
+
+//track the permission status.
+//if permission is true, set the editor readOnly to false, theme will be changed as well.
+dbRefRoomPermission.on('value', snap => {
+    const permission_holder = snap.val().userId;
+    console.log("permission holder: ", permission_holder);
+    if (permission_holder !== userId && permission === false) {
+    } else {
+        permission = permission_holder === userId;
+        dbRefCode.once('value', codeSnap => {
+            const content = codeSnap.val();
+            //global var permission
+            code_input.value = content['content'];
+            editor.setValue(code_input.value);
+            editor.setOption("readOnly", !permission);
+            if(permission) {
+                editor.setOption("theme", 'darcula');
+            } else {
+                editor.setOption("theme", 'lucario');
+            }
+        });
+    }
+});
+
+
+//initialize code input
+dbRefCode.once('value', snap => {
+    const content = snap.val();
+    code_input.value = content['content'];
+    editor.setValue(code_input.value);
+});
+
+//synchronize code input
+dbRefCode.on('value', snap => {
+    const content = snap.val();
+    code_input.value = content['content'];
+    if (!permission)
+        editor.setValue(code_input.value);
+});
+
+//同步user list, 因为user list是由子elem构成的所以按照子elem来增删（用户进入/离开房间）
+dbRefUserList.on('child_added', snap => {
+    const user_obj = snap.val();
+    const avatar    = $("<img>", {class: "avatar img-circle b2-avatar",id: "user_avatar" + user_obj.authorId, src:user_obj.avatar});
+    const user = $("<span>", {class: "user-name-span"}).text(user_obj.username);
+    const add = $("<li>", {id: snap.key}).append($("<div>", {class: "user-list"}).append(avatar, user));
+    $("ul[id='user_list']").append(add);
+});
+
+dbRefUserList.on('child_removed', snap => {
+    const liToRemove = document.getElementById(snap.key);
+    liToRemove.remove();
+});
+
+//Ask for permission need to be after user list, because it needs the elements user list generates.
+dbRefAskForPermission.on('child_added', snap => {
+    if (permission) {
+        const request_user = snap.key;
+        if (snap.val()) {
+            const button = $("<button>", {class: "pass-submission-btn"}).text("Pass Permission").click({passTo: request_user}, passPermission);
+            $("div ul[id=user_list] li[id=" + request_user + "] div").append(button);
+        }
+    }
+});
+
+dbRefAskForPermission.on('child_removed', snap => {
+    if (permission) {
+        const request_user = snap.key;
+        if (snap.val()) {
+            $("div ul[id=user_list] li[id=" + request_user + "] div button").remove();
+        }
+    }
+});
+//同步comment list，与user list类似，但是comment可以改，user不行
+dbRefCommentList.on('child_added', snap => {
+    const comment_obj = snap.val();
+    const avatar    = $("<img>", {class: "avatar img-circle b2-avatar",id: "avatar" + comment_obj.authorId, src:comment_obj.avatar});
+    const user_name = $("<span>", {class: "b2-commentname"}).text(comment_obj.author);
+    const content   = $("<p>", {class: "b2-commentcontent"}).text(comment_obj.content);
+    const up        = $("<div>", {class: "ci-up"}).append(avatar, user_name);
+    const down      = $("<div>", {class: "ci-down"}).append(content);
+    const li = $("<li>", {id: snap.key, class: "comment-item"}).append($("<div>", {class:"ci-div"}).append(up, down));
+    $("ul[id='comment_list']").append(li);
+    if (comment_obj.position) {
+        const pos = comment_obj.position;
+        const code = comment_obj.code;
+        if (code === editor.getRange(pos.from, pos.to)) {
+            comment_dict[snap.key] = editor.getDoc().markText({
+                line: pos.from.line,
+                ch: pos.from.ch
+            }, {
+                line: pos.to.line,
+                ch: pos.to.ch
+            }, {
+                className: "cm-marker",
             });
         }
+    }
+});
+
+dbRefCommentList.on('child_removed', snap => {
+    const liToRemove = document.getElementById(snap.key);
+    let comment_marker = comment_dict[snap.key];
+    console.log("Comment removed");
+    liToRemove.remove();
+    comment_marker.clear();
+});
+
+//TODO change.
+dbRefCommentList.on('child_changed', snap => {
+    const liChanged = document.getElementById(snap.key);
+    liChanged.innerText = JSON.stringify(snap.val());
+});
+
+window.onbeforeunload = function (e) {
+    const host = window.location.host;
+    const path = window.location.pathname;
+    const url = 'http://' + host + path + '/users';
+    // const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/users';
+    $.ajax({
+        url: url,
+        method: 'delete',
+        dataType: 'json',
+    }).done(function (data) {
+        console.log(data);
+    }).fail(function (xhr, status) {
+        console.log('Fail: ' + xhr.status + ', msg: ' + xhr.responseJSON.msg);
     });
-
-
-    //initialize code input
-    dbRefCode.once('value', snap => {
-        const content = snap.val();
-        code_input.value = content['content'];
-        editor.setValue(code_input.value);
-    });
-
-    //synchronize code input
-    dbRefCode.on('value', snap => {
-        const content = snap.val();
-        code_input.value = content['content'];
-        if (!permission)
-            editor.setValue(code_input.value);
-    });
-
-    //synchronize code display
-    dbRefCode.on('value', snap => {
-        const content = snap.val();
-        code.innerText = content['content'];
-    });
-
-    //同步user list, 因为user list是由子elem构成的所以按照子elem来增删（用户进入/离开房间）
-    //TODO add a ask-for-permission button
-     dbRefUserList.on('child_added', snap => {
-         const user_obj = snap.val();
-         const avatar    = $("<img>", {class: "avatar img-circle b2-avatar",id: "user" + snap.key, src: user_obj.avatar, css: ""});
-         const user = $("<span>", {class: "user-name-span"}).text(user_obj.username);
-         const add = $("<li>", {id: snap.key}).append($("<div>", {class: "user-list"}).append(avatar,user));
-         $("ul[id='user_list']").append(add);
-     });
-
-     dbRefUserList.on('child_removed', snap => {
-            const liToRemove = document.getElementById(snap.key);
-            liToRemove.remove();
-        });
-
-    //Ask for permission need to be after user list, because it needs the elements user list generates.
-    dbRefAskForPermission.on('child_added', snap => {
-        if (permission) {
-            const request_user = snap.key;
-            if (snap.val()) {
-             console.log("request", request_user);
-             const button = $("<button>", {class: "pass-submission-btn"}).text("Pass Permission").click({passTo: request_user}, passPermission);
-             $("div ul[id=user_list] li[id=" + request_user + "] div").append(button);
-            }
-        }
-    });
-
-    dbRefAskForPermission.on('child_removed', snap => {
-         if (permission) {
-             const request_user = snap.key;
-             if (snap.val()) {
-                 console.log("request", request_user);
-                 const a = "div ul[id=user_list] li[id=" + request_user + "]";
-                 $("div ul[id=user_list] li[id=" + request_user + "] div button").remove();
-             }
-         }
-     });
-    //同步comment list，与user list类似，但是comment可以改，user不行
-    //TODO 改成仅当comment 模式的时候显示
-    dbRefCommentList.on('child_added', snap => {
-        const comment_obj = snap.val();
-        const avatar    = $("<img>", {class: "avatar img-circle b2-avatar",id: "avatar" + comment_obj.authorId});
-        const user_name = $("<span>", {class: "b2-commentname"}).text(comment_obj.author);
-        const content   = $("<p>", {class: "b2-commentcontent"}).text(comment_obj.content);
-        const up        = $("<div>", {class: "ci-up"}).append(avatar, user_name);
-        const down      = $("<div>", {class: "ci-down"}).append(content);
-        const li = $("<li>", {id: snap.key, class: "comment-item"}).append($("<div>", {class:"ci-div"}).append(up, down));
-        $("ul[id='comment_list']").append(li);
-        if (comment.position) {
-            const pos = comment.position;
-            const code = comment.code;
-            if (code === editor.getRange(pos.from, pos.to)) {
-                let comment_marker = editor.getDoc().markText({
-                    line: pos.from.line,
-                    ch: pos.from.ch
-                }, {
-                    line: pos.to.line,
-                    ch: pos.to.ch
-                }, {
-                    css: "background-color : #ff7"
-                });
-                comment_dict[snap.key] = comment_marker;
-            }
-        }
-    });
-
-    dbRefCommentList.on('child_removed', snap => {
-        const liToRemove = document.getElementById(snap.key);
-        let comment_marker = comment_dict[snap.key];
-        console.log("Comment removed");
-        liToRemove.remove();
-        comment_marker.clear();
-    });
-
-    //TODO change.
-    dbRefCommentList.on('child_changed', snap => {
-        const liChanged = document.getElementById(snap.key);
-        liChanged.innerText = JSON.stringify(snap.val());
-    });
-
-    window.onbeforeunload = function (e) {
-         const host = window.location.host;
-         const path = window.location.pathname;
-         const url = 'http://' + host + path + '/users';
-         // const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/users';
-         $.ajax({
-             url: url,
-             method: 'delete',
-             dataType: 'json',
-         }).done(function (data) {
-             console.log(data);
-         }).fail(function (xhr, status) {
-             console.log('Fail: ' + xhr.status + ', msg: ' + xhr.responseJSON.msg);
-         });
-     };
+};
 //function to update code to firebase
 function updateCode() {
     firebase.database().ref().child('code/').child(roomId).update(
@@ -200,8 +193,9 @@ function run() {
     const result = document.getElementById('result');
     const host = window.location.host;
     const path = window.location.pathname;
-    const url = 'http://' + host + path + '/run';
-    // const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/run';
+    //TODO change
+    // const url = 'http://' + host + path + '/run';
+    const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/run';
     console.log(url);
     $.ajax({
         url: url,
@@ -286,26 +280,32 @@ function passPermission(event) {
 
 //TODO Delete after testing.
 function resetPermission() {
-
     const host = window.location.host;
     const path = window.location.pathname;
     const passToId = '5bb053d0efdfca206dc66b3f';
     const url = 'http://' + host + path + '/permission/' + passToId;
     // const url = 'http://127.0.0.1:3000/api/coderooms/' + roomId + '/permission/' + passToId;
     $.ajax({
-     url: url,
-     method: 'delete',
-     dataType: 'json',
+        url: url,
+        method: 'delete',
+        dataType: 'json',
     }).done(function (data) {
-     console.log(data);
+        console.log(data);
     }).fail(function (xhr, status) {
-     console.log('Fail: ' + xhr.status + ', msg: ' + xhr.responseJSON.msg);
+        console.log('Fail: ' + xhr.status + ', msg: ' + xhr.responseJSON.msg);
     });
 }
 
-//TODO change mode between coding and commenting
+//TODO merge with comment-block and user-block's display control part
 function modeChange() {
     comment_mode = !comment_mode;
+    let marker_list=document.getElementsByClassName("cm-marker");
+    let length = marker_list.length;
+    let css = null;
+    if (comment_mode) {
+        css = "background-color : #ff7"
+    }
+    while(length--) {
+        marker_list[length].style.cssText=css;
+    }
 }
-//TODO delete user in the user_list when leave room.(close the window/jump to other pages.)
-//还需要将classroom1，user1, comment1之类的改成classroomID, userID, commentID
