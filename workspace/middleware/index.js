fe4983569be1b6ce41823ee4f33dcded0002bd0c
Shen Qu <53157020@qq.com>
2018-10-09 15:41:30 +0800

    delete

--- workspace/middleware/index.js
+++ workspace/middleware/index.js
@@ -6,22 +6,22 @@ module.exports = {
         }
         req.flash("error", "You must be signed in to do that!");
         res.redirect('/login');
-    },
-    isOwner: function(req, res, next){
-        if(req.user){
-            Coderoom.findById(req.params.id, function(err, found_coderoom){
-               if(found_coderoom.permitted_user.id.equals(req.user.id)
-                   || found_coderoom.author.id.equals(req.user.id)
-                   || req.user.isAdmin){
-                   next();
-               } else {
-                   req.flash("error", "You don't have permission to do that!");
-                   res.redirect("/coderooms/" + req.params.id);
-               }
-            });
-        } else {
-            req.flash("error", "You need to be signed in to do that!");
-            res.redirect("/login");
-        }
     }
+    // isOwner: function(req, res, next){
+    //     if(req.user){
+    //         Coderoom.findById(req.params.id, function(err, found_coderoom){
+    //            if(found_coderoom.permitted_user.id.equals(req.user._id)
+    //                || found_coderoom.author.id.equals(req.user.id)
+    //                || req.user.isAdmin){
+    //                next();
+    //            } else {
+    //                req.flash("error", "You don't have permission to do that!");
+    //                res.redirect("/coderooms/" + req.params.id);
+    //            }
+    //         });
+    //     } else {
+    //         req.flash("error", "You need to be signed in to do that!");
+    //         res.redirect("/login");
+    //     }
+    // }
 };
