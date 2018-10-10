3d6846c05bd19ba91043972185935b58c274cfbe
Zheng Lu <z5135652@cse.unsw.edu.au>
2018-10-10 13:42:06 +1100

    bug fixed

--- workspace/controllers/pythonExecutor.js
+++ workspace/controllers/pythonExecutor.js
@@ -1,32 +1,24 @@
 const fs = require('fs');
 const spawn = require('child_process').spawn;
+const exec  = require('child_process').exec;
 const async = require('async');
 module.exports = (res, roomID, code) => {
     let filename = 'pythonFiles/' + roomID + ".py";
     let runCommand = "./" + filename;
-    console.log("TEST: ", filename);
     fs.writeFileSync(filename, code, function(err){
-        if(err) {console.log(err);}
+        if(err) { console.log(err);}
         else { console.log("WriteFile Finished."); }
     });
+    let process = spawn('python3',[runCommand]);
     let ret = {};
-    let process = spawn('python',[runCommand]);
-    async.parallel({
-        stdout: function(callback) {
-            process.stdout.on('data', function(data) {
-                callback(null, data.toString());
-            });
-        },
-        stderr: function(callback) {
-            process.stderr.on('data', function(data) {
-                callback(null, data.toString());
-            });
-        }
-        }, function(err, results) {
-            console.log(results);
-            res.json({output: results.stdout, err: results.stderr})
-        }
-    )
+    process.stdout.on('data', function(data) {
+        ret['output'] = data.toString();
+    });
+    process.stderr.on('data', function(data) {
+        ret['err'] = data.toString();
+    });
+    process.on('exit', function() {
+        res.json(ret);
+    });
 };
 
-//基本完成，剩下文件过滤，import可能有问题？没试过
